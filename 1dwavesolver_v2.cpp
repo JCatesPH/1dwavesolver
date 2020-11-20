dc1106c36272aba7c86deb3399ad7ed6f79007bf
jmcates <jmcates@crimson.ua.edu>
2020-11-20 13:27:27 -0700

    Few versions with varying input parameters

--- 1dwavesolver_v2.cpp
+++ 1dwavesolver_v2.cpp
@@ -14,15 +14,15 @@
 
 const double c = GSL_CONST_MKSA_SPEED_OF_LIGHT;
 const double eps0 = GSL_CONST_MKSA_VACUUM_PERMITTIVITY;
-const int nx = 10000;
-const double dx = 1e-9;
+const int nx = 5000;
+const double dx = 1e-6;
 const int nt = 10000;
-const double dt = 1e-18;
+const double dt = 1e-15;
 const double sigma = dx*1e-1;
 const double lamb0 = 5e-7;
 double k0 = 2*M_PI/lamb0;
 const int nk = 5;
-double dk = k0*1e-2;
+double dk = k0*1e-1;
 double shift = nx/10*dx;
 double omega = 2*M_PI*c/lamb0;
 const double I0 = 1e3;
@@ -32,33 +32,31 @@ double A0 = sqrt(2*I0/(eps0*c));
 
 using namespace std;
 
-complex<double> f(double x, complex<double> *k, complex<double> *Ak, double s){
+complex<double> f(double x, complex<double> *k, double s){
     complex<double> Im (0.0,1.0);
     complex<double> sum = 0.0;
     for (int i=0; i<nk; i++){
-        sum = sum + Ak[i] * exp(Im * k[i] * x);
+        sum = sum + A0 * cos(k[i] * x);
     }
     return sum;
 }
-complex<double> g(double x, complex<double> *k, complex<double> *Ak, double omega, double s){
+complex<double> g(double x, complex<double> *k, double omega, double s){
     complex<double> Im (0.0,1.0);
     complex<double> sum = 0.0;
     for (int i=0; i<nk; i++){
-        sum = sum - k[i] * Ak[i] * c * exp(Im * k[i] * x);
+        sum = sum - A0 * k[i] * c * sin(k[i] * x);
     }
     return sum;
 }
 
-void initialconds(double *x, complex<double> *kx, complex<double> *Ak, complex<double> *u, complex<double> *ut, double omega, double s){
+void initialconds(double *x, complex<double> *kx, complex<double> *u, complex<double> *ut, double omega, double s){
     for (int i=0; i<nk; i++){
-        kx[i] = k0 - (nk / 2 - i) * dk;
-        Ak[i] = A0 * exp(-(kx[i]-k0)*(kx[i]-k0));
-        cout << "i = " << i << ", kx = " << kx[i] << "\n";
+        kx[i] = k0 - (nx / 2 - i) * dk;
     }
     for(int i=0; i<nx; i++){
         x[i] = (i-nx/4)*dx;
-        u[i] = f(x[i], kx, Ak, shift);
-        ut[i] = g(x[i], kx, Ak, omega, shift);
+        u[i] = f(x[i], kx, shift);
+        ut[i] = g(x[i], kx, omega, shift);
     }
     
 }
@@ -67,7 +65,6 @@ void initialconds(double *x, complex<double> *kx, complex<double> *Ak, complex<d
 int main(){
     double *x = (double *)malloc(nx*sizeof(double));
     complex<double> *kx = (complex<double> *)malloc(nk*sizeof(complex<double>));
-    complex<double> *Ak = (complex<double> *)malloc(nk*sizeof(complex<double>));
     complex<double> *u = (complex<double> *)malloc(nx*sizeof(complex<double>));
     complex<double> *ut = (complex<double> *)malloc(nx*sizeof(complex<double>));
     double alpha = c * dt / dx;
@@ -82,37 +79,37 @@ int main(){
     cout << "nt = " << nt << "\n";
     cout << "dt = " << dt << "\n";
 
-    initialconds(x, kx, Ak, u, ut, omega, shift);
+    initialconds(x, kx, u, ut, omega, shift);
 
     FILE *fp;
 
     fp = fopen("initialdistribution.csv", "w+");
     fprintf(fp, "x,Re[E],Im[E]\n");
     for(int i=0; i<nx; i++){
-        fprintf(fp, "%f,%f,%f\n",x[i],real(u[i]),imag(u[i]));
+        fprintf(fp, "%f,%f,%f,\n",x[i],real(u[i]),imag(u[i]));
     }
     fclose(fp);
 
     for(int i=0; i<nx; i++){
-        complex<double> fi = f(x[i],kx,Ak,shift);
-        complex<double> gi = g(x[i],kx,Ak,omega,shift);
+        complex<double> fi = f(x[i],kx,shift);
+        complex<double> gi = g(x[i],kx,omega,shift);
         complex<double> fim1;
         complex<double> fip1;
 
         if (i == 0)
         {
             fim1 = 0.0;
-            fip1 = f(x[i+1],kx,Ak,shift);
+            fip1 = f(x[i+1],kx,shift);
         }
         else if (i == nx - 1)
         {
-            fim1 = f(x[i-1],kx,Ak,shift);
+            fim1 = f(x[i-1],kx,shift);
             fip1 = 0.0;
         }
         else 
         {
-            fim1 = f(x[i-1],kx,Ak,shift);
-            fip1 = f(x[i+1],kx,Ak,shift);
+            fim1 = f(x[i-1],kx,shift);
+            fip1 = f(x[i+1],kx,shift);
         }
         Et1[i] = dt * gi + (1 - alpha * alpha) * fi + 0.5 * alpha * alpha * (fim1 + fip1);
         Et2[i] = Et1[i] - 2 * dt * gi;
--- plot.py
+++ plot.py
@@ -5,17 +5,34 @@ import pandas as pd
 
 initarr = pd.read_csv('initialdistribution.csv')
 # %%
-initarr.plot('x',['Re[u]','Im[u]'])
-# %%
-E_20 = pd.read_csv('data/Ep_20.csv')
-E_20.plot('x',['Re[E]','Im[E]'])
+initarr.plot('x',['Re[E]','Im[E]'])
+
+Emax = np.amax(np.abs(initarr['Re[E]'].to_numpy()))
+#Emax = 3e9
 
 # %%
-E_480 = pd.read_csv('data/Ep_480.csv')
-E_480.plot('x',['Re[E]','Im[E]'])
-# %%
-for i in range(0,1000,100):
+for i in range(0,10000,200):
     tmp = pd.read_csv('data/Ep_' + str(i) + '.csv')
-    ax = tmp.plot('x',['Re[E]','Im[E]'])
-    fig = ax.get_figure()
+    xarr = tmp['x'].to_numpy()
+    Ereal = tmp['Re[E]'].to_numpy()
+    Eimag = tmp['Im[E]'].to_numpy()
+    Earr = Ereal + 1j * Eimag
+    Ek = np.fft.fft(Earr)
+    freq = np.fft.fftfreq(Ek.shape[-1])
+    
+    fig, axs = plt.subplots(1,2,figsize=(12,4))
+    axs[0].plot(xarr,Ereal,xarr,Eimag)
+    axs[0].set_ylim(-3*Emax,3*Emax)
+    axs[0].legend(['Re[E(x)]','Im[E(x)]'])
+    ti = "{:.1E} s".format(i*1e-15)
+    axs[0].set_title(r'|E| for $t=$' + ti)
+
+    axs[1].plot(freq, np.real(Ek), freq, np.imag(Ek))
+    axs[1].set_title(r'$E(k)$ for $t=$' + ti)
+    axs[1].legend(['Re[E(k)]','Im[E(k)]'])
+
     fig.savefig('figs/Ep_' + str(i) + '.png')
+
+   
+
+# %%
